85b74e5de79a34c5578e6eb04d1b4752c563a92b
DragonhandX <31867624+DragonhandX@users.noreply.github.com>
2018-10-11 15:06:10 -0400

    Hero controls implemented

--- Assets/_Scripts/Hero.cs
+++ Assets/_Scripts/Hero.cs
@@ -3,14 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Hero : MonoBehaviour {
+    static public Hero S; // singleton
+
+    [Header("Set in Inspector")]
+    // These fields control the movement of the ship
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+
+    [Header("Set Dynamically")]
+    public float shieldLevel = 1;
 
 	// Use this for initialization
 	void Start () {
-		
+		if (S == null)
+        {
+            S = this;
+        }
+        else
+        {
+            Debug.LogError("Hero.Awake() - Attempted to assign a second Hero.S!");
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        // Pull in information from the Input class
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        // Change transform.position based on the axes
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        // Rotate the ship to make it feel more dynamic
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
 	}
 }
