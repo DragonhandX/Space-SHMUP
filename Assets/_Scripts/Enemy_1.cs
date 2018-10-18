92c0c72cac632f0752351d1ad5d255ea5d251f6c
DragonhandX <31867624+DragonhandX@users.noreply.github.com>
2018-10-18 14:11:56 -0400

    enemy 1 now moves
    new scripts generate their own update method, which needed to be deleted to prevent automatically overriding it's parent version.

--- Assets/_Scripts/Enemy_1.cs
+++ Assets/_Scripts/Enemy_1.cs
@@ -15,29 +15,31 @@ public class Enemy_1 : Enemy {
 
 	// Use this for initialization
 	void Start () {
+        // Set x0 to the initial x position of Enemy_1
         x0 = pos.x;
 
         birthTime = Time.time;
 	}
 
+    // Override the Move function on Enemy
     public override void Move()
     {
+        // Because pos is a propert, you can't directly set pos.x
+        //  so get the pos as an editable Vector3
         Vector3 tempPos = pos;
-
+        // theta adjusts based on time
         float age = Time.time - birthTime;
         float theta = Mathf.PI * 2 * age / waveFrequency;
         float sin = Mathf.Sin(theta);
         tempPos.x = x0 + waveWidth * sin;
         pos = tempPos;
 
+        // rotate a bit about y
         Vector3 rot = new Vector3(0, sin * waveRotY, 0);
         this.transform.rotation = Quaternion.Euler(rot);
 
+        // base.Move() still handles the movement down in y
         base.Move();
     }
 
-    // Update is called once per frame
-    void Update () {
-		
-	}
 }
