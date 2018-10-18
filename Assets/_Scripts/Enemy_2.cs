a32ddab56b55abf65c58080d31650190f74cd3fa
DragonhandX <31867624+DragonhandX@users.noreply.github.com>
2018-10-18 16:02:41 -0400

    Weapon research 50%
    strted work on adding more weapon types

--- Assets/_Scripts/Enemy_2.cs
+++ Assets/_Scripts/Enemy_2.cs
@@ -2,15 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_2 : MonoBehaviour {
+public class Enemy_2 : Enemy {
+    [Header("Set in Inspector: Enemy_2")]
+    // Determines how much the Sine wave will affect movement
+    public float sinEccentricity = 0.6f;
+    public float lifeTime = 10;
+
+    [Header("Set Dynamically: Enemy_2")]
+    // Enemy_2 uses a Sin wave to modify a 2-point linear interpolation
+    public Vector3 p0;
+    public Vector3 p1;
+    public float birthTime;
 
 	// Use this for initialization
 	void Start () {
-		
+        // Pick any point on the left side of the screen
+        p0 = Vector3.zero;
+        p0.x = -bndCheck.camWidth - bndCheck.radius;
+        p0.y = Random.Range(-bndCheck.camHeight, bndCheck.camHeight);
+
+        // Pick any point on the right side of the screen
+        p1 = Vector3.zero;
+        p1.x = bndCheck.camWidth + bndCheck.radius;
+        p1.y = Random.Range(-bndCheck.camHeight, bndCheck.camHeight);
+
+        // possibly swap sides
+        if (Random.value > 0.5f)
+        {
+            // Setting the .x of each point to its negative will move it to the other side of the screen
+            p0.x *= -1;
+            p1.x *= -1;
+        }
+
+        birthTime = Time.time;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public override void Move()
+    {
+        float u = (Time.time - birthTime) / lifeTime;
+
+        if (u > 1)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        u = u + sinEccentricity * (Mathf.Sin(u * Mathf.PI * 2));
+
+        pos = (1 - u) * p0 + u * p1;
+    }
 }
--- Assets/_Scripts/Main.cs
+++ Assets/_Scripts/Main.cs
@@ -5,11 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class Main : MonoBehaviour {
     static public Main S;
+    static Dictionary<WeaponType, WeaponDefinition> WEAP_DICT;
 
     [Header("Set in Inspector")]
     public GameObject[] prefabEnemies;
     public float enemySpawnPerSecond = 0.5f;
     public float enemyDefaultPadding = 1.5f;
+    public WeaponDefinition[] weaponDefinitions;
 
     private BoundsCheck bndCheck;
 
@@ -20,6 +22,12 @@ public class Main : MonoBehaviour {
         bndCheck = GetComponent<BoundsCheck>();
         // Invoke SpawnEnemy() once (in 2 seconds, based on default values)
         Invoke("SpawnEnemy", 1f / enemySpawnPerSecond);
+
+        WEAP_DICT = new Dictionary<WeaponType, WeaponDefinition>();
+        foreach(WeaponDefinition def in weaponDefinitions)
+        {
+            WEAP_DICT[def.type] = def;
+        }
     }
 
     public void SpawnEnemy()
@@ -57,6 +65,15 @@ public class Main : MonoBehaviour {
     {
         // Reload _Scene_0 to restart the game
         SceneManager.LoadScene("_Scene_0");
+    }
+
+    static public WeaponDefinition GetWeaponDefinition( WeaponType wt)
+    {
+        if (WEAP_DICT.ContainsKey(wt))
+        {
+            return (WEAP_DICT[wt]);
+        }
+        return (new WeaponDefinition());
     }
 
     // Use this for initialization
